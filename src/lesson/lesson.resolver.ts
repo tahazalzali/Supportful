c34b4125f176124fa4661238a4f741f847253d4f
taha zalzali <tahazalzaly51@gmail.com>
2024-04-18 22:05:54 +0300

    Buil app module main components according to official documentaion, create lesson module, running on localhost graphql

---
+++ src/lesson/lesson.resolver.ts
@@ -0,0 +1,20 @@
+import { Resolver,Query } from "@nestjs/graphql";
+import { LessonType } from "./lesson.type";
+
+@Resolver(of=>LessonType)
+
+export class LessonResolver{
+
+    @Query(returns =>LessonType)
+lesson(){
+    return{
+        id:'fdsjhsdf',
+        name:'Physics Class',
+        startDate: (new Date()).toISOString(),
+        endDate:(new Date()).toISOString()
+
+    }
+
+}
+
+ }
