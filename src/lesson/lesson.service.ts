62aad458d2421498531436e0e7b6254c8028a561
taha zalzali <tahazalzaly51@gmail.com>
2024-04-18 22:46:20 +0300

    create lesson service and ingect using typeorm

--- src/lesson/lesson.service.ts
+++ src/lesson/lesson.service.ts
@@ -1,4 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Lesson } from './lesson.entity';
+import { Repository } from 'typeorm';
+import {v4 as uuid }from 'uuid';
 
 @Injectable()
-export class LessonService {}
+export class LessonService {
+    constructor(
+        @InjectRepository(Lesson) private lessonRepository: Repository<Lesson>
+    ) {
+    }        
+    createLesson(name: string, startDate: string, endDate: string) {
+        const lesson = this.lessonRepository.create({
+            name: 'Math Class',
+            strtDate: (new Date()).toISOString(),
+            endDate: (new Date()).toISOString(),
+            id: uuid().toString()
+        });
+        return this.lessonRepository.save(lesson);
+    }
+}
